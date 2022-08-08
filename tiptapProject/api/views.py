2bd37b4e3e0adb7a22005c8f173177eeed448b1d
4-002602 <seolym@hanyang.ac.kr>
2022-08-08 23:09:38 +0900

    Feat: 매물 조회 API 주소 필터링 및 정렬 기능 추가

--- tiptapProject/api/views.py
+++ tiptapProject/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from api.serializers import RoomSerializer
 from app.models import Room
 
+from app.utils import string_to_list
+
 class RoomAPIView(ReadOnlyModelViewSet):
     """RoomAPIView
     
@@ -18,8 +20,19 @@ class RoomAPIView(ReadOnlyModelViewSet):
     queryset = Room.objects.all().select_related('roomInfo')
     serializer_class = RoomSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+    """def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+        start, center, end = string_to_list(self.request.query_params.get('location'))
+        queryset = queryset.filter(
+            roomInfo__basicInfo_location_x__gte=start[0],
+            roomInfo__basicInfo_location_x__lte=end[0],
+            roomInfo__basicInfo_location_y__gte=start[1],
+            roomInfo__basicInfo_location_y__lte=end[1]
+        )
+        return queryset"""
+
+    """def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
         
         page = self.paginate_queryset(queryset)
         if page:
@@ -28,5 +41,29 @@ class RoomAPIView(ReadOnlyModelViewSet):
         
         serializer = self.get_serializer(queryset, many=True)
         
+        response_data = {"total": len(serializer.data), "rooms": serializer.data}
+        return Response(response_data)"""
+    
+    def retrieve(self, request, *args, **kwargs):
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        """
+        left_bottom, middle, right_top = string_to_list(request.GET.get("location","[[-100,-100],[0,0],[200,200]]")) #location없으면 기본 값 가져옴
+        check = self.filter_queryset(self.get_queryset()).extra(
+            select={'manhattan_distance': f'ABS(basicInfo_location_x - {middle[0]}) + ABS(basicInfo_location_y - {middle[1]})'},
+            where={f'''basicInfo_location_x > {left_bottom[0]} and basicInfo_location_y > {left_bottom[1]}
+                   and basicInfo_location_x < {right_top[0]} and basicInfo_location_y <{right_top[1]}'''}
+        ).order_by('manhattan_distance')
+
+        queryset = self.filter_queryset(check)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
         response_data = {"total": len(serializer.data), "rooms": serializer.data}
         return Response(response_data)
