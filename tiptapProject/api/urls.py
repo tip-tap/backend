df9f1662799970cc33ac0a8f881c6a80c3e90886
4-002602 <seolym@hanyang.ac.kr>
2022-08-08 23:08:15 +0900

    Fix: 매물 조회 API 주소 변경
    api/urls.py의 GET에 대한 액션을 list에서 retrieve로 변경

--- tiptapProject/api/urls.py
+++ tiptapProject/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('rooms/', views.RoomAPIView.as_view(actions={"get": "list"})),
+    path('rooms/', views.RoomAPIView.as_view(actions={"get": "retrieve"})),
 ]
