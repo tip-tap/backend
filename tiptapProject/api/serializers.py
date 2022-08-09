3f4b60631c66df2e1ea912da7acb899f2fdd1e33
4-002602 <seolym@hanyang.ac.kr>
2022-08-09 09:58:54 +0900

    Fix: 매물 조회 시 세부 정보를 표시하지 않도록 수정

--- tiptapProject/api/serializers.py
+++ tiptapProject/api/serializers.py
@@ -2,10 +2,25 @@ from rest_framework import serializers
 from app.models import BrokerAgency, CheckList, Image, Room, RoomInfo, Tag
 
 # RoomInfoSerializer: Created by @ssanghy on checklist
-class RoomInfoSerializer(serializers.ModelSerializer):
+class RoomInfoForRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomInfo
-        exclude = ('roominfo_id', )
+        exclude = (
+            'roominfo_id',
+            'detailInfo_is_moldy',
+            'detailInfo_is_leak',
+            'detailInfo_is_bug',
+            'detailInfo_is_crack',
+            'detailInfo_soundproof',
+            'detailInfo_window_size',
+            'detailInfo_main_direction',
+            'detailInfo_ventilator',
+            'detailInfo_ventilation',
+            'detailInfo_external_noise',
+            'detailInfo_water_pressure',
+            'detailInfo_drainage',
+            'detailInfo_hot_water',
+        )
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -21,7 +36,7 @@ class BrokerAgencySerializer(serializers.ModelSerializer):
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    roomInfo = RoomInfoSerializer()
+    roomInfo = RoomInfoForRoomSerializer()
     tag = TagSerializer(many=True, read_only=True)
     thumbnail = serializers.SerializerMethodField()
     images = serializers.SerializerMethodField()
