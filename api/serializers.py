5d901227f28ae191c03203caea511ee7306dd778
byeongjaeSon <byeongjae.dev@gmail.com>
2022-08-09 12:13:01 +0900

    Fix: 관심매물 조회 시 세부 정보는 직렬화하지 않도록 수정

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,25 @@ from rest_framework import serializers
 
 from app.models import Room, RoomInfo, Interest, Tag, BrokerAgency
 
-class RoomInfoSerializer(serializers.ModelSerializer):
+class RoomInfoForInterestSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomInfo
-        exclude = ('roominfo_id',)
+        exclude = (
+            'roominfo_id',
+            'detailInfo_is_moldy',
+            'detailInfo_is_leak',
+            'detailInfo_is_bug',
+            'detailInfo_is_crack',
+            'detailInfo_soundproof',
+            'detailInfo_window_size',
+            'detailInfo_main_direction',
+            'detailInfo_ventilator',
+            'detailInfo_ventilation',
+            'detailInfo_external_noise',
+            'detailInfo_water_pressure',
+            'detailInfo_drainage',
+            'detailInfo_hot_water',
+        )
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,7 +33,7 @@ class BrokerAgencySerializer(serializers.ModelSerializer):
         fields = ('brokerAgency_id', 'brokerAgency_name', 'brokerAgency_number1')
 
 class RoomSerializer(serializers.ModelSerializer):
-    roomInfo = RoomInfoSerializer()
+    roomInfo = RoomInfoForInterestSerializer()
     tag = TagSerializer(many=True, read_only=True)
     thumbnail = serializers.SerializerMethodField()
     def get_thumbnail(self, obj):
